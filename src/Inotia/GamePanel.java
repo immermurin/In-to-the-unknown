4562b77dbb64ff2301e0fde873bc1040f0370bb2
Immergreen <105064773+immermurin@users.noreply.github.com>
2025-02-26 16:33:06 +0800

    d

--- src/Inotia/GamePanel.java
+++ src/Inotia/GamePanel.java
@@ -1,6 +1,30 @@
 package Inotia;
 
-public class GamePanel 
-{
+public class GamePanel extends javax.swing.JPanel {
+
+    public GamePanel() 
+    {
+        initComponents();
+    }
+
     
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 857, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 489, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
 }
