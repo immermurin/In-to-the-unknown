f48fc47539f278cd97e23932495bfe3e03f29bea
Immergreen <105064773+immermurin@users.noreply.github.com>
2025-03-07 19:19:47 +0800

    Starting from zero

--- src/Inotia/Main.java
+++ src/Inotia/Main.java
@@ -1,24 +1,9 @@
 package Inotia;
 
-import javax.swing.*;
-
-
 public class Main 
 {
     public static void main(String[] args) 
     {
-        JFrame window = new JFrame("Game");
-        BossFight b = new BossFight(window);
-        
-        window.setUndecorated(true); //Full screen
-        window.setExtendedState(JFrame.MAXIMIZED_BOTH); //Full screen
-        
-        window.add(b);
-        //window.pack();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(false);
-        
-        window.setVisible(true);
-        b.requestFocusInWindow();
+        System.out.println("Hi");
     } 
 }
